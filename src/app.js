f31c1ba32b4e6eb262739d74e7858127fd9e0f5c
Andy Farmer <afarmer@rawnet.com>
2016-09-02 16:11:33 +0100

    add series view and finalise prototyping

--- src/app.js
+++ src/app.js
@@ -27,16 +27,54 @@ App.controller('CategoryController', function($scope, $routeParams) {
 
     $scope.category = $routeParams.categoryId;
 
+    $scope.bannerImage = 'http://placehold.it/1300x400';
+
     $scope.programs = [];
+
+    $scope.featured = [];
 
     $scope.hasData.promise.then(function() {
         $scope.globalData.forEach(function(item){
-          if(!$scope.programs.includes(item.SeriesName) && item.ProgramTypeSS === $scope.category){
-            $scope.programs.push(item.SeriesName);
+          if(item.ProgramTypeSS === $scope.category){
+            var program = {
+              id : item.SeriesID, 
+              name : item.SeriesName
+            }
+            $scope.programs.push(program);
+            if(item.IsFeatured === "TRUE"){
+              var fullBannerPath = item.BannerURL.split('/');
+              program.thumbnailImage = fullBannerPath[fullBannerPath.length - 1];
+              $scope.featured.push(program);
+            }
           }
         });
     });
  
+});
+
+App.controller('SeriesController', function($scope, $routeParams) {
+
+  $scope.hasData.promise.then(function() {
+    $scope.globalData.forEach(function(item){
+      if(item.SeriesID === $routeParams.seriesId){
+        var fullBannerPath = item.BannerURL.split('/'),
+            bannerFileName = fullBannerPath[fullBannerPath.length - 1];
+
+        $scope.bannerImage = bannerFileName;
+        $scope.series = item.SeriesName;
+        $scope.seasonCount = item.SeasonCount;
+        $scope.episodeCount = item.EpisodeCount;
+        $scope.channel = item.PrimaryNetworkName;
+        $scope.productionCompany = item.ProductionCompany;
+        $scope.promoImage = item.PromoImageURL;
+        $scope.trailerImage = item.TrailerImageURL;
+        $scope.spanishImage = item.SpanishImageURL;
+
+      }
+    });
+  });
+
+
 });
 
 App.config(function($routeProvider, $locationProvider) {
@@ -48,6 +86,10 @@ App.config(function($routeProvider, $locationProvider) {
   .when('/category/:categoryId', {
     templateUrl: '/category.html',
     controller: 'CategoryController'
+  })
+  .when('/series/:seriesId', {
+    templateUrl: '/series.html',
+    controller: 'SeriesController'
   });
 
   $locationProvider.html5Mode(true);
