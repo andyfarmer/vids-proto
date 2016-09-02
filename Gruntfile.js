d0cdf3d68cfc6c681be70fc4cdf3a99e40b90474
Andy Farmer <afarmer@rawnet.com>
2016-09-02 14:30:14 +0100

    add images to task to scrape for images

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,12 @@
 module.exports = function(grunt) {
+  var images =[];
+
+  var bannersData = grunt.file.readJSON('build/seriesdata.json');
+
+  bannersData.forEach(function(image){
+    images.push(image.BannerURL);
+  });
+
   grunt.initConfig({
     http: {
       grab_data: {
@@ -16,12 +24,21 @@ module.exports = function(grunt) {
         src: ['src/seriesdata.csv'],
         dest: 'build/seriesdata.json'
       }
+    },
+    download: {
+      banner_images: {
+        src:      images,
+        dest:     'build/series-banners/'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-http');
   grunt.loadNpmTasks('grunt-convert');
+  grunt.loadNpmTasks('grunt-http-download');
 
   grunt.registerTask('data', ['http', 'convert']);
+
+  grunt.registerTask('images', ['download']);
 
 };
